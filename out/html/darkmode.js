4302e5a1dcd2bd14277678a94f6bdaedee9d302f
LifeIsCheese <micoo0708@protonmail.com>
2025-07-07 23:36:39 -0600

    update

--- out/html/darkmode.js
+++ out/html/darkmode.js
@@ -1,19 +1,20 @@
 let darkmode = localStorage.getItem('darkmode')
-const themeSwitch = document.getElementById('theme-switch')
 
 const enableDarkmode = () => {
   document.body.classList.add('darkmode')
   localStorage.setItem('darkmode', 'active')
+  document.getElementById('theme_yes').checked = true
 }
 
 const disableDarkmode = () => {
   document.body.classList.remove('darkmode')
-  localStorage.setItem('darkmode', null)
+  localStorage.setItem('darkmode', 'inactive')
+  document.getElementById('theme_no').checked = true
 }
 
-if(darkmode === "active") enableDarkmode()
-
-themeSwitch.addEventListener("click", () => {
-  darkmode = localStorage.getItem('darkmode')
-  darkmode !== "active" ? enableDarkmode() : disableDarkmode()
-})
+// Apply saved theme on load
+if (darkmode === 'active') {
+  enableDarkmode()
+} else {
+  disableDarkmode()
+}
