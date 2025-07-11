135d7e57bed081b5039a39fd2f8c2e67f6383502
LifeIsCheese <micoo0708@protonmail.com>
2025-07-10 19:35:48 -0600

    toggle

--- out/html/darkmode.js
+++ out/html/darkmode.js
@@ -18,3 +18,30 @@ if (darkmode === 'active') {
 } else {
   disableDarkmode()
 }
+
+let eventImg = localStorage.getItem('eventImg')
+
+const enableEventImg = () => {
+  localStorage.setItem('eventImg', 'active');
+  let images = document.querySelectorAll('.event-container img, .face-status-container img');
+  for (let img of images) {
+    img.style.display = 'block';
+  }
+  document.getElementById('eventImg_yes').checked = true;
+};
+
+const disableEventImg = () => {
+  localStorage.setItem('eventImg', 'inactive');
+  let images = document.querySelectorAll('.event-container img, .face-status-container img');
+  for (let img of images) {
+    img.style.display = 'none';
+  }
+  document.getElementById('eventImg_no').checked = true;
+};
+
+// Apply saved theme on load
+if (eventImg === 'active') {
+  enableEventImg()
+} else {
+  disableEventImg()
+}
