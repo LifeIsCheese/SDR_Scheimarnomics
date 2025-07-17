1d18c8a0b3159b4f218c0fa93e61b5dc262ea6d3
LifeIsCheese <micoo0708@protonmail.com>
2025-07-16 20:55:12 -0600

    even more vsiaul i hate buttons i hate css

--- out/html/game.js
+++ out/html/game.js
@@ -500,6 +500,9 @@ window.changeTab('status_right.Actions_rb', 'Actions_rb', 'right');
 function bruning() {
 
 d.bruning_plot = "successful"
+d.kpd_party_leader == "Conciliators"
+d.kpd_relation = 100;
+d.left_strength = 60;
 
 }
 
